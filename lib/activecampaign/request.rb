fe22384415efdf08e04fa1a410b7eea13953a1c1
gildemberg-santos <gildemberg.santos@gmail.com>
2022-09-07 08:47:16 -0300

    remover metodo de validação da classe

--- lib/activecampaign/request.rb
+++ lib/activecampaign/request.rb
@@ -4,14 +4,14 @@
 # Activecampaign::Request.new.get
 module Activecampaign
   class Request
-    def initialize(url = nil, token = nil)
-      uri(url)
-      token(token)
+    def initialize(url:, token:)
+      @url = Activecampaign.validade_uri(url)
+      @token = Activecampaign.validade_token(token)
       @response = nil
     end
 
     def post(payload = nil)
-      validate_payload(payload)
+      Activecampaign.validate_payload(payload)
 
       @response = execute_request(method: :post, payload: payload)
       return json if status_code == 200
@@ -51,29 +51,6 @@ module Activecampaign
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       request = requisition_settings(method: method, payload: payload)
       http.request(request)
-    end
-
-    def validate_payload(payload = nil)
-      return nil if payload.is_a?(NilClass)
-      raise Activecampaign::InvalidPayloadError, "Payload must be a Hash '#{payload.class}'" unless payload.is_a?(Hash)
-      raise Activecampaign::InvalidPayloadError, "Payload is required" if payload.blank?
-    end
-
-    def uri(url)
-      raise Activecampaign::InvalidURIError, "Url is must be a String '#{url.class}'" unless url.is_a?(String)
-
-      @url ||= URI.parse(url)
-      raise Activecampaign::InvalidURIError, "Url is invalid '#{url}'" if @url.host.blank?
-    rescue URI::InvalidURIError => error_uri
-      raise Activecampaign::InvalidURIError, "Invalid URI '#{error_uri.message}'"
-    end
-
-    def token(token)
-      raise Activecampaign::InvalidTokenError, "Token must be a String '#{token.class}'" unless token.is_a?(String)
-      raise Activecampaign::InvalidTokenError, "Token is required" if token.blank?
-      raise Activecampaign::InvalidTokenError, "Token is invalid '#{token}'" unless token.match?(/[A-Za-z0-9]{72}/)
-
-      @token ||= token
     end
   end
 end
